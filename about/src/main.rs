225c5634f2f830670efe4472983bb72fcb08c825
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-08-13 10:39:59 +0000

    Initial commit

---
+++ about/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("A tool to visually represent and analyze Ruby codebases, providing interactive maps of modules, classes, methods, and relationships to improve code comprehension, navigation, and collaboration.");
+}
